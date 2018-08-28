1f4b36b03e05ba63a436e0c850a46a351b591915
VasiliyTurchenko <34768220+VasiliyTurchenko@users.noreply.github.com>
2018-08-28 12:59:24 +0300

    correct names

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,41 +6,42 @@ import java.util.*;
 
 public class Main {
 
-    private static byte[] Pattern = {9,8,7,6,5,4,3,2,1,0};
+    private static byte[] pattern = {9,8,7,6,5,4,3,2,1,0};
     private static  int bufSize = 100000000;
-    private static final long magicP = Integer.MAX_VALUE;
+    private static final long MAGIC_P = Integer.MAX_VALUE;
     private static final long X = 666 /*Integer.MAX_VALUE*/;
+    private static final int FIT_POSITION = 10000;
 
     private static long[] powers;
 
     private static long collisions;
 
 
-    public static long mod(long a, long MOD) {
-        return ((a % MOD) + MOD) % MOD;
+    public static long mod(long a, long modulo) {
+        return ((a % modulo) + modulo) % modulo;
     }
 
-    public static long HashIt(byte[] s, int start, int len) {
-        long h = 0;
+    public static long hashIt(byte[] str, int start, int len) {
+        long hash = 0;
         int ii = 0;
         for (int i = start; i < start + len; i++) {
             long hd;
-            hd = mod((long)s[i]*powers[ii], magicP);
-            h = mod(h + hd, magicP);
+            hd = mod((long)str[i]*powers[ii], MAGIC_P);
+            hash = mod(hash + hd, MAGIC_P);
             ii++;
         }
-        return h;
+        return hash;
     }
 
-    public static boolean isEqual(byte[] pattern, byte[] text, int i) {
+    public static boolean isEqual(byte[] pattern, byte[] text, int startPosition) {
         boolean result = true;
         for(int j = 0; j < pattern.length; j++) {
-            if ((i + j) == text.length) {
+            if ((startPosition + j) == text.length) {
                 collisions++;
                 result = false;
                 return result;
             }
-            if (pattern[j] != text[j + i]) {
+            if (pattern[j] != text[j + startPosition]) {
                 collisions++;
                 result = false;
                 return result;
@@ -49,27 +50,27 @@ public class Main {
         return result;
     }
 
-    public static  ArrayList<Integer> RKSearch(byte[] p, byte[] t) {
+    public static  ArrayList<Integer> searchRK(byte[] pattern, byte[] text) {
         ArrayList<Integer> ans = new ArrayList<Integer>();
-        int lp = p.length;
-        int lt = t.length;
-        long lastpow = powers[lp - 1];
-        long HP = HashIt(p, 0, lp);
+        int lp = pattern.length;
+        int lt = text.length;
+        long lastPow = powers[lp - 1];
+        long hp = hashIt(pattern, 0, lp);
         int k = lt - lp;
-        long HT = HashIt(t, k, lp);
-        if (HP == HT) {
-            if (isEqual(p, t, k) == true) {
+        long ht = hashIt(text, k, lp);
+        if (hp == ht) {
+            if (isEqual(pattern, text, k) == true) {
                 ans.add(k);
             }
         }
 
         for (int i = k - 1; i >= 0; i--) {
-            long A = mod(lastpow * (long) t[i + lp], magicP);
-            long HM = mod((HT - A), magicP);
-            HM = mod(HM * X, magicP);
-            HT = mod(HM + (long) t[i], magicP);
-            if (HP == HT) {
-                if (isEqual(p, t, i) == true) {
+            long a = mod(lastPow * (long) text[i + lp], MAGIC_P);
+            long hm = mod((ht - a), MAGIC_P);
+            hm = mod(hm * X, MAGIC_P);
+            ht = mod(hm + (long) text[i], MAGIC_P);
+            if (hp == ht) {
+                if (isEqual(pattern, text, i) == true) {
                     ans.add(i);
                 }
             }
@@ -81,24 +82,24 @@ public class Main {
     public static void main(String[] args) {
 
         long tStart = System.currentTimeMillis();
-        byte[] T = new byte[bufSize];
+        byte[] text = new byte[bufSize];
         for (int i = 0; i < bufSize; i++) {
-            T[i] = (byte)i;
+            text[i] = (byte)i;
         }
-        for (int i = 0; i < Pattern.length; i++) {
-            T[i + 10000] = Pattern[i];
+        for (int i = 0; i < pattern.length; i++) {
+            text[i + FIT_POSITION] = pattern[i];
         }
 
 
         collisions = 0;
-        powers = new long[Pattern.length];
+        powers = new long[pattern.length];
         powers[0] = 1L;
 
-        for (int i = 1; i < Pattern.length; i++) {
-            powers[i] = mod(X*powers[i-1], magicP);
+        for (int i = 1; i < pattern.length; i++) {
+            powers[i] = mod(X*powers[i-1], MAGIC_P);
         }
 
-        ArrayList<Integer> ans = RKSearch(Pattern, T);
+        ArrayList<Integer> ans = searchRK(pattern, text);
         for (int i= ans.size() - 1; i >= 0 ; i-- ) {
             System.out.printf("%d ", ans.get(i));
         }
